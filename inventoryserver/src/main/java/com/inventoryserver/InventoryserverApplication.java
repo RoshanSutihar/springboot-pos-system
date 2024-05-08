8eeccbc4e1536569adf78a54a828817bea43e592
Roshan Sutihar <roshansutihar@gmail.com>
2024-05-08 13:29:32 -0500

    Initial Template Commit

---
+++ inventoryserver/src/main/java/com/inventoryserver/InventoryserverApplication.java
@@ -0,0 +1,13 @@
+package com.inventoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryserverApplication.class, args);
+	}
+
+}
---
+++ inventoryserver/src/test/java/com/inventoryserver/InventoryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
